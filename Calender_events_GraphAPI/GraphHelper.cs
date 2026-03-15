d59d1b4d3a5b1b79ff6a893125e07c70e7b765eb
pavithradogiparthi <86222542+pavithradogiparthi@users.noreply.github.com>
2026-03-15 15:15:28 +0530

    Add files via upload

---
+++ Calender_events_GraphAPI/GraphHelper.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Azure.Core;
+using Azure.Identity;
+using Microsoft.Graph;
+using Microsoft.Graph.Me.SendMail;
+using Microsoft.Graph.Models;
+
+namespace Microsoft_Graph
+{
+    public class GraphHelper
+    {
+        // Settings object
+        private static Settings? _settings;
+        // User auth token credential
+        private static DeviceCodeCredential? _deviceCodeCredential;
+        // Client configured with user authentication
+        private static GraphServiceClient? _userClient;
+
+        public static void InitializeGraphForUserAuth(Settings settings,
+            Func<DeviceCodeInfo, CancellationToken, Task> deviceCodePrompt)
+        {
+            _settings = settings;
+
+            var options = new DeviceCodeCredentialOptions
+            {
+                ClientId = settings.ClientId,
+                TenantId = settings.TenantId,
+                DeviceCodeCallback = deviceCodePrompt,
+            };
+
+            _deviceCodeCredential = new DeviceCodeCredential(options);
+
+            _userClient = new GraphServiceClient(_deviceCodeCredential, settings.GraphUserScopes);
+        }
+        public static async Task<string> GetUserTokenAsync()
+        {
+            // Ensure credential isn't null
+            _ = _deviceCodeCredential ??
+                throw new System.NullReferenceException("Graph has not been initialized for user auth");
+
+            // Ensure scopes isn't null
+            _ = _settings?.GraphUserScopes ?? throw new System.ArgumentNullException("Argument 'scopes' cannot be null");
+
+            // Request token with given scopes
+            var context = new TokenRequestContext(_settings.GraphUserScopes);
+            var response = await _deviceCodeCredential.GetTokenAsync(context);
+            return response.Token;
+        }
+        public static Task<User?> GetUserAsync()
+        {
+            // Ensure client isn't null
+            _ = _userClient ??
+                throw new System.NullReferenceException("Graph has not been initialized for user auth");
+
+            return _userClient.Me.GetAsync((config) =>
+            {
+                // Only request specific properties
+                config.QueryParameters.Select = new[] { "displayName", "mail", "userPrincipalName" };
+            });
+        }
+        public static Task<MessageCollectionResponse?> GetInboxAsync()
+        {
+            // Ensure client isn't null
+            _ = _userClient ??
+                throw new System.NullReferenceException("Graph has not been initialized for user auth");
+
+            return _userClient.Me
+                // Only messages from Inbox folder
+                .MailFolders["Inbox"]
+                .Messages
+                .GetAsync((config) =>
+                {
+                    // Only request specific properties
+                    config.QueryParameters.Select = new[] { "from", "isRead", "receivedDateTime", "subject" };
+                    // Get at most 25 results
+                    config.QueryParameters.Top = 25;
+                    // Sort by received time, newest first
+                    config.QueryParameters.Orderby = new[] { "receivedDateTime DESC" };
+                });
+        }
+        public static Task<EventCollectionResponse?> GetCalenderAsync()
+        {
+            // Ensure client isn't null
+            _ = _userClient ??
+                throw new System.NullReferenceException("Graph has not been initialized for user auth");
+
+             return _userClient.Me.Events.GetAsync((config) =>
+            {
+                config.QueryParameters.Select = new[] { "subject" };
+                config.QueryParameters.Top = 20;
+                config.QueryParameters.Orderby = new[] { "createdDateTime DESC" };
+            });
+
+              
+        }
+
+        
+
+    }
+}
