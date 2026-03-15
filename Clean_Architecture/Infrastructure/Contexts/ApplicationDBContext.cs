749498e51f4e8e02f8cf967b41c4c5927add62da
pavithradogiparthi <86222542+pavithradogiparthi@users.noreply.github.com>
2026-03-15 15:25:03 +0530

    Add files via upload

---
+++ Clean_Architecture/Infrastructure/Contexts/ApplicationDBContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Contexts
+{
+   public  class ApplicationDBContext:DbContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options):base(options) { 
+         
+        }
+        public DbSet<Property> Properties =>Set<Property>();
+        public DbSet<Image> Images =>Set<Image>();
+    }
+}
---
+++ Clean_Architecture/Infrastructure/Repositories/ImageRepo.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Application.Repository;
+using Domain;
+using Infrastructure.Contexts;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Repositories
+{
+    public class ImageRepo : IImageRepo
+    {
+        private readonly ApplicationDBContext _context;
+        public ImageRepo(ApplicationDBContext context)
+        {
+            
+            _context = context;
+        }
+        public async Task AddNewAsync(Image image)
+        {
+            await _context.AddAsync(image);
+           await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(Image image)
+        {
+            _context.Remove(image);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<List<Image>> GetAllAsync()
+        {
+         return await _context.Images.ToListAsync();
+        }
+
+        public  async Task<Image> GetByIdAsync(int id)
+        {
+              return await _context.Images.Where(x => x.Id == id).FirstOrDefaultAsync();
+        }
+
+        public async Task UpdateAsync(Image image)
+        {
+           _context.Images.Update(image);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
